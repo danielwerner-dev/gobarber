8c230097dc163b211e3a64fa68c35d1e21129d43
daniel <danielortiz3@gmail.com>
2019-10-30 01:42:14 -0300

    gobarber API - Redis/Email/Apoointments

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c50ca2ccf17822',
+    pass: 'a9378e4e280873',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
