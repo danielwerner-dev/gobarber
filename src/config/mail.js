ed8da47934088ceefa5174b964dd98ab1c861dc8
daniel <danielortiz3@gmail.com>
2019-10-30 22:31:22 -0300

    gobarber API

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'c50ca2ccf17822',
-    pass: 'a9378e4e280873',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'GoBarber <noreply@gobarber.com>',
